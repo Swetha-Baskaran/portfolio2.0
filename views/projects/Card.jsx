b3210d2f3d0071d6fd2230faf5f4e82848a7a0a7
swethasambothi@gmail.com <Swetha-Baskaran>
2022-11-11 21:58:06 +0530

    mobile ui changes

--- views/projects/Card.jsx
+++ views/projects/Card.jsx
@@ -10,9 +10,14 @@ import classes from "./style.module.scss";
 
 const IndividualProject = () => {
 	return (
-		<Card sx={{ maxWidth: 360 }} className={classes["individual-card"]}>
+		// <Card sx={{ maxWidth: 360 }} className={classes["individual-card"]}>
+		<Card sx={{ maxWidth: 360 }} className={classes["individual-card"]} style={customStyle.individualCard}>
 			<div className={classes["card-img-hold"]}>
-				<img src="/assets/gatsby-hospital-landing-page.png" alt="" className={classes["card-img"]} />
+				<img
+					src='/assets/gatsby-hospital-landing-page.png'
+					alt=''
+					className={classes["card-img"]}
+				/>
 				<div className={classes["link-btns"]}>
 					<FiGithub className={classes["btns"]} />
 					<RiExternalLinkFill className={classes["btns"]} />
@@ -33,15 +38,22 @@ const IndividualProject = () => {
 					<span>js</span>
 				</p>
 			</CardContent>
-			<CardContent>
+			{/* <CardContent>
 				<Typography variant='body2'>
 					This impressive paella is a perfect party dish and a fun meal to, if you like.
 				</Typography>
-			</CardContent>
+			</CardContent> */}
 		</Card>
 	);
 };
 
 export default IndividualProject;
 
-const customStyle = {};
+const customStyle = {
+	individualCard: {
+		margin: "2rem",
+		borderRadius: "30px 0 30px 0",
+		boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+		border: "0.2px solid #8080802c",
+	},
+};
