40f35a98416ea0d571eedf18697e0390c6d9ed02
Swetha-Baskaran <swethasambothi@gmail.com>
2023-01-03 12:32:53 +0530

    version 1.1

--- views/projects/Projects.jsx
+++ views/projects/Projects.jsx
@@ -1,30 +1,39 @@
-import { useState } from "react";
+import {useState} from "react";
 import Pagination from "@mui/material/Pagination";
 import IndividualProject from "./Card";
 import classes from "./style.module.scss";
 import Grid from "@mui/material/Grid";
 import Titles from "../../components/Titles";
 import Button from "@mui/material/Button";
-import { Typography } from "@mui/material";
-import { ProjectUrl } from "../../data/projects"
-import { useRouter } from 'next/router'
+import {Typography} from "@mui/material";
+import {ProjectUrl} from "../../data/projects";
+import {useRouter} from "next/router";
 
 const Projects = () => {
 	const Router = useRouter();
-	const Projects = Router.pathname === "/Projects" ? ProjectUrl : ProjectUrl.slice(0,5)
+	const Projects =
+		Router.pathname === "/Projects" ? ProjectUrl : ProjectUrl.slice(0, 5);
 	return (
 		<>
-			<div className={classes["project-container"]} id="Projects">
+			<div className={classes["project-container"]} id='Projects'>
 				<Titles name='My Recent Works.' />
-				<Typography data-aos="fade-down">Here are few projects I have worked on recently.</Typography>
+				<Typography data-aos='fade-down'>
+					Here are few projects I have worked on recently.
+				</Typography>
 				<Grid container className={classes["grid"]}>
 					{Projects.map((e, index) => {
 						return <IndividualProject key={index} data={e} />;
 					})}
 				</Grid>
-				<Button variant='outlined' href='/Projects' className={classes["link"]}>
-					See More
-				</Button>
+				{Router.pathname !== "/Projects" && (
+					<Button
+						variant='outlined'
+						href='/Projects'
+						className={classes["link"]}
+					>
+						See More
+					</Button>
+				)}
 			</div>
 		</>
 	);
