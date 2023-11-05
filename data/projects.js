b08b5568318c5469f0190d8b578bcd3e2e35a61f
Swetha Baskaran <74134426+Swetha-Baskaran@users.noreply.github.com>
2023-11-05 19:31:06 +0530

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -16,7 +16,7 @@ export const ProjectUrl = [
 		group: "1",
 		link: "https://www.kankariyasupermarket.in",
 		img: `/assets/projects/kankariya.png`,
-		technologies: ["Vite Js", "MUI", "Codemirror"],
+		technologies: ["AWS", "Vite Js", "Node Js", "Mongo DB"],
 		details:
 			"Your Ultimate Online Supermarket: Streamlining All Your Household Needs with Seamless Ordering. Discover the convenience of one-click shopping for all your daily essentials.",
 	},
